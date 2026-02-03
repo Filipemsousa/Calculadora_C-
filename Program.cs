0046a4dbfb390d0286bef779d421af5ab846f101
025.030174 <025.030174@C305-3045425>
2026-02-03 11:30:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Aula05;
+    public class Program
+    {
+        public static void Main()
+    {
+        Console.WriteLine("==============Calculadroa bem simples====");
+        Console.WriteLine("Digite o número 1: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o número 2: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = number1 + number2;
+        int subtration = number2 - number1;
+        int multiplication = number1 * number2;
+        int division = number2 / number1;
+        int module = number2 % number1;
+
+        Console.WriteLine("O valor da soma é: " + sum);
+        Console.WriteLine("O valor da sub:"+ subtration);
+        Console.WriteLine("O valor da multiplicaçã é: " + multiplication);
+        Console.WriteLine("O valor da divisão é: " + division);
+
+        Console.WriteLine("O valor do módulo é: " + module);
+    }
+
+}
