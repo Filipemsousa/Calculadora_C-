254c956e7e81c3e4da5d9c46be51e213f6b1412f
FILIPE MENEZES SOUSA <025.030174@universidadesenaicimatec.edu.br>
2026-02-04 09:04:10 -0300

    Nova alteração

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     {
         public static void Main()
     {
-        //Calculadora simples....
+        //Calculadora simples em C#
         Console.WriteLine("==============Calculadroa bem simples====");
         Console.WriteLine("Digite o número 1: ");
         int number1 = Convert.ToInt32(Console.ReadLine());
@@ -17,12 +17,14 @@
         int division = number2 / number1;
         int module = number2 % number1;
 
+        Console.WriteLine("==============Resultado====");
         Console.WriteLine("O valor da soma é: " + sum);
         Console.WriteLine("O valor da sub:"+ subtration);
         Console.WriteLine("O valor da multiplicaçã é: " + multiplication);
         Console.WriteLine("O valor da divisão é: " + division);
 
         Console.WriteLine("O valor do módulo é: " + module);
+        Console.WriteLine("Fim do programa");
     }
 
 }
